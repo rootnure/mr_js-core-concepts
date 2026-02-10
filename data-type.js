79f042963b8e22fd38043d6598760c3fe4e940bb
rootnure <nur.diu.2791@gmail.com>
2026-02-10 21:30:27 +0600

    Data types in JS

---
+++ data-type.js
@@ -0,0 +1,12 @@
+// JS is a dynamic typed programming language
+let data = 4;
+data = "Hello";
+data = true;
+data = { name: "Kafi" };
+data = [1, 5, 8];
+data = undefined;
+data = null;
+function func() {}
+
+console.log(typeof data);
+console.log(typeof func);
